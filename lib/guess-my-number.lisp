19b36f91573d2e057018d8304f20b13bc4c072b0
ArakiTakaki <araki_takaki@yahoo.co.jp>
2020-02-24 00:03:15 +0900

    ok

---
+++ lib/guess-my-number.lisp
@@ -0,0 +1,26 @@
+
+; 予測される最小値
+(defparameter *small* 1)
+
+; 予測される最大値
+(defparameter *big* 100)
+
+; 予測を吐き出す
+(defun guess-my-number ()
+  ; 1bit右シフトさせ *small* + *big* の平均を算出させている。
+  (ash (+ *small* *big*) -1))
+
+; 予測された値より小さい場合の処理
+(defun smaller () 
+  ; guess-my-number から予測された値から -1 した値をbigへセットする
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number))
+
+; 予測された値より大きい場合の処理
+(defun bigger () 
+  ; guess-my-number から予測された値から +1 した値をsmallへセットする
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number))
+
+
+
